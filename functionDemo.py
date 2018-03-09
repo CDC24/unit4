216273e4e13497105b952feb01d3b8d88418b8bd
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-09 08:23:31 -0500

    Updated from Brython Server: 3/9/2018 8:23:31 AM

--- functionDemo.py
+++ functionDemo.py
@@ -5,17 +5,29 @@
 
 # fuctions are pretty much anything with parentheses
 
+"""
 #define a new functions
 def hw():
     print ("Hello World")
 
 #to use the fnctn:
-#hw()
+hw()
+"""
 
-
+"""
 def double(thingToDouble):
     print(thingToDouble*2)
     
 double(156)
 double("w")
 double(False)
+double(True)
+"""
+
+def bigger(a,b):
+    if a>b:
+        print (a)
+    if a<b:
+        print (b)
+    else:
+        print ("aaaaaaa")
