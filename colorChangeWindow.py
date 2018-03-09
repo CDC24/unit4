11213484f4eaf20866dde8e408a1f60ef6a72a65
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-09 08:47:55 -0500

    Create colorChangeWindow.py

---
+++ colorChangeWindow.py
@@ -0,0 +1,15 @@
+#Caleb Callaway
+#3/9/18
+#colorChangeWindow.py - uses functions to reprint strings vertically
+
+from ggame import *
+#the asterisk imports the whole thing
+
+
+red = Color(0xFF0000,1) # (RGB code, opacity)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0X00000000,1)
+
+blackOutline = LineStyle(1,black)
+redRectangle = RectangleAsset(100,300, blackOutline, red) #(width,height,outline,fill)
