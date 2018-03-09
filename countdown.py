8cf66e18a19baf784c07e1b9134f0af08073860f
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-09 08:42:19 -0500

    Updated from Brython Server: 3/9/2018 8:42:18 AM

--- countdown.py
+++ countdown.py
@@ -1,3 +1,15 @@
 #Caleb Callaway
 #3/9/18
 #countdown.py - uses functions to count down
+
+
+
+def countdown(num):
+    for i in range (-num, 0):
+    print (-i)
+    if i == -1:
+        print ("BOOOM! GOT 'EM")
+        
+q = int(input("Enter a number: "))
+
+countdown (q)
