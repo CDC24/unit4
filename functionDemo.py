2e2a8f050378bd486b0519d5056159383bb5934c
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-09 08:16:14 -0500

    Updated from Brython Server: 3/9/2018 8:16:14 AM

--- functionDemo.py
+++ functionDemo.py
@@ -1,1 +1,10 @@
+#Caleb Callaway
+#3/9/18
+#functionDemo.py - how to write your own functions
 
+
+# fuctions are pretty much anything with parentheses
+
+#define a new functions
+def hw():
+    print ("Hello World")
