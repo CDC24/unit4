d8c6c7fe47803748a9fe9d15e8cb500be9446087
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-09 08:38:31 -0500

    Updated from Brython Server: 3/9/2018 8:38:30 AM

--- printStars.py
+++ printStars.py
@@ -1,3 +1,12 @@
 #Caleb Callaway
 #3/9/18
 #printStars.py - uses functions to print stars
+
+
+def printStars(e):
+    for i in range(1,  e+1):
+       print (" "*(e-i)," *"*i)
+       
+e = int(input("Enter a number "))
+
+printStars(e)
