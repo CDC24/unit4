3bfddd0172a2d9166fc5a97c14cab7b894a25b0e
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-09 08:45:06 -0500

    Updated from Brython Server: 3/9/2018 8:45:05 AM

--- vprint.py
+++ vprint.py
@@ -1,3 +1,12 @@
 #Caleb Callaway
 #3/9/18
 #vprint.py - uses functions to reprint strings vertically
+
+
+def vprint(words):
+    for ch in (words):
+        print (ch)
+        
+txt = input("Enter some text, please, sir... ")
+
+vprint(txt)
