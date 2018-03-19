b4b02a3a6abf069e7fc72baa2241efc42330ec58
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-19 08:32:32 -0400

    Updated from Brython Server: 3/19/2018 8:32:32 AM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -4,10 +4,16 @@
 
 from ggame import *
 
-red = Color(0xFF0000,1) # (RGB code, opacity)
+red = Color(0xFF0000,1)
 green = Color(0x00FF00,1)
 blue = Color(0x0000FF,1)
 black = Color(0X00000000,1)
 
 blackOutline = LineStyle(1,black)
-redRectangle = RectangleAsset(100,300, blackOutline, red) #(width,height,outline,fill)
+redRectangle = RectangleAsset(1000,1000, blackOutline, red)
+
+
+Sprite(redRectangle)
+
+
+App().run()
