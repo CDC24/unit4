206b09abfa7a3ad372dd7f8a723a3be87a3ef320
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-19 08:15:03 -0400

    Updated from Brython Server: 3/19/2018 8:15:03 AM

--- returnDemo.py
+++ returnDemo.py
@@ -2,6 +2,17 @@
 #3/19/18
 #returnDemo.py - how to make functions return a value
 
+#returns random even numbers
+
+from random import randint
+
+def randEven(low,high):
+    n=randint(low,high)
+    while n%2 !=2:
+        n = randint (low,high)
+    print (n)
+    
+randEven(10,20) #test
 
 
-print ("YAOOOAAH")
+# print (randint(low/2,high/2)*2)       dun't really work
