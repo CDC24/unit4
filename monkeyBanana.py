2c2d39330a2ad6db8a00b136c24eb1313babaaa0
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-22 08:08:23 -0400

    Updated from Brython Server: 22/03/2018 08:08:23

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -9,12 +9,12 @@ from ggame import *
 if __name__ == "__main__":       #those are 2 underscores not 1      this is so u can run the main part by itself
 
 
-green = Color(0x006600,1)
-brown = Color(0x0000FF,1)
-yellow = Color(0X00000000,1)
+    green = Color(0x006600,1)
+    brown = Color(0x0000FF,1)
+    yellow = Color(0X00000000,1)
 
-jungleBox = RectangleAsset(1500,1500,LineStyle(1,green),green)
+    jungleBox = RectangleAsset(1500,1500,LineStyle(1,green),green)
 
-sprite(jungleBox)
+    Sprite(jungleBox)
 
-App().run()
+    App().run()
