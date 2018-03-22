f586c50d5dc8b95ac0217cd8037a96ea50b06de0
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-22 08:12:37 -0400

    Updated from Brython Server: 22/03/2018 08:12:37

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -7,15 +7,15 @@
 from ggame import *
 
 #constants:
-ROWS = 20
-COLS = 40
+ROWS = 40
+COLS = 80
 CELLSIZE = 20
 
 
 if __name__ == "__main__":       #those are 2 underscores not 1      this is so u can run the main part by itself
 
 
-    green = Color(0x006600,1)
+    green = Color(0x000660,1)
     brown = Color(0x0000FF,1)
     yellow = Color(0X00000000,1)
 
