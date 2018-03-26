3e663f8aafd9db21170b3dde735c12a0d564e3c3
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-26 08:14:43 -0400

    Updated from Brython Server: 26/03/2018 08:14:42

--- warmUp2.py
+++ warmUp2.py
@@ -3,3 +3,16 @@
 #warmUp2.py
 
 # function that returns true if a number is prime and false otherwise
+
+
+def prime(num):
+    for i in range (2, num-1):
+    if num%i==0:
+        print (False)
+        
+        
+        
+        
+num = int(input("Enter a number: "))
+
+print (prime(num))
