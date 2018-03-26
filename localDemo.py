0520cfd71e970ff201e2dca9ba428a491de950ed
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-26 08:08:55 -0400

    Update localDemo.py

--- localDemo.py
+++ localDemo.py
@@ -1,3 +1,13 @@
 #Caleb Callaway
 #3/26/18
-#localDemo.py - Local Variables
+#localDemo.py - Local Variables???
+
+def f():
+    x = 20    #this is a local variable - exists within function and not elsewhere
+    y = 200
+
+x = 2
+f()
+print (x)
+print(y)
+
