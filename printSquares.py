96ac3f5de52062aceb6024598707082845f937cf
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-28 08:23:45 -0400

    Updated from Brython Server: 28/03/2018 08:23:38

--- printSquares.py
+++ printSquares.py
@@ -1,3 +1,11 @@
 #Caleb Callaway
 #3/26/18
 #printSquares.py - prints squares using functions
+
+def printSquares(rows,cols):
+    print("+"+"---+---+"*cols)
+    for i in range (0,rows):
+        print("|"+"   |   |"*cols)
+        print("+"+"---+---+"*cols)
+    
+printSquares(5,5)
