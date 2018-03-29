2037952ce5fc38d812f17b77416eb9ee60f75850
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-29 08:26:00 -0400

    Updated from Brython Server: 29/03/2018 08:25:59

--- warmUp12.py
+++ warmUp12.py
@@ -1,3 +1,18 @@
 #Caleb Callaway
 #3/29/18
 #warmUp12.py - finds gcf of two numbers
+
+
+
+def GCF(num1,num2):
+    if num1 > num2:                     #You don't actually need this part;
+        smaller = num2
+    else:
+        smaller = num1
+    for i in range (smaller,0,-1):      #Just write 'num1' or 'num2' here instead of smaller
+        if num1%i==0 and num2%i==0:
+            return (i)
+
+print (GCF(12,15))
+print (GCF(5,9))
+print (GCF(16,64))
