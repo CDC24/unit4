6eb640e267a10e953471aa3fef28d7700a78611a
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-29 08:29:55 -0400

    Updated from Brython Server: 29/03/2018 08:29:54

--- stringUnion.py
+++ stringUnion.py
@@ -1,3 +1,15 @@
 #Caleb Callaway
 #3/28/18
 #stringUnion.py - blends two words
+
+
+def stringUnion(word1,word2):
+    twoword=("word1"+"word2")
+    newword=("")
+    for ch in twoword:
+        if ch not in newword:
+            newword +=(ch)
+    return(newword)
+            
+            
+print(stringUnion(Mississippi,Pensylvania))
