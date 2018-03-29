2685bf1cceb2ae4d04899b9c2f6b7496295afc1c
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-29 08:06:20 -0400

    Updated from Brython Server: 29/03/2018 08:06:19

--- recursionDemo.py
+++ recursionDemo.py
@@ -1,3 +1,14 @@
 #Caleb Callaway
 #3/29/18
-#recursionDemo.py - wot
+#recursionDemo.py - recursive version of countdown
+
+
+def countdownr(n):
+    if n == 0:                 # establishes 'base case'
+        print("KERCHOW!")
+    else:
+        print (n)
+        countdownr(n-1)
+        
+        
+countdownr(0)
