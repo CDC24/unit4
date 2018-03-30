687c96982f2111fbb9e29885db25a4175b13ba0e
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-03-30 08:38:38 -0400

    Updated from Brython Server: 30/03/2018 08:38:38

--- triangle.py
+++ triangle.py
@@ -21,20 +21,13 @@ def triangle(x1,y1,x2,y2,x3,y3):
     b= distance(x2,y2,x3,y3)
     c= distance(x3,y3,x1,y1)
     s= (0.5*(a+b+c))
-    return (sqrt(s(s-a)(s-b)(s-c)))
+    return (sqrt(s*(s-a)*(s-b)*(s-c)))
 
-
-
+print(triangle(x1,y1,x2,y2,x3,y3))
 
 """
-def triangle(x1,y1,x2,y2,x3,y3):
-    a= (sqrt((x2-x1)**2+(y2-y1)**2))
-    b= (sqrt((x3-x2)**2+(y3-y2)**2))
-    c= (sqrt((x1-x3)**2+(y1-y3)**2))
-    s= (0.5*(a+b+c))
-    return (sqrt(s(s-a)(s-b)(s-c)))
-
-
 print(triangle(3,4,-5,2,-7,1))
-
 """
+
+
+
