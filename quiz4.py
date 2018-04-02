3b1aef925861d7cb51c14e8dbde523e748dc05d2
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-04-02 08:34:03 -0400

    Updated from Brython Server: 02/04/2018 08:34:02

--- quiz4.py
+++ quiz4.py
@@ -4,30 +4,30 @@
 
 
 
-def count(x):
+def count(x):                           # function 1
     for i in range (1,x+1):
         print (i)
 
-count(7)  # test of function 1
+count(7)                                # test of function 1
 
 
-def excitedPrint(word):
+def excitedPrint(word):                 # function 2
     print (word.upper(),"!!!")
     
-excitedPrint("radical")
+excitedPrint("radical")                 # test of function 3
 
 
-def firstLetter(letters):
+def firstLetter(letters):               # function 3
     for ch in letters:
         return ch
         
-print (firstLetter("sfrgtbbr"))
+print (firstLetter("sfrgtbbr"))         # test of function 3
 
 
-def repeats(a,b,c):
+def repeats(a,b,c):                     # function 4
     if a==b or b==c or c==a:
         return(True)
     else:
         return(False)
     
-print (repeats(5,6,5))
+print (repeats(5,6,5))                  # test of function 4
